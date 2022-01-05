41790e741e3692d5107d659b50601b8fab82bf86
illya8800 <illya.gopak2000@gmail.com>
2022-01-05 23:33:09 +0300

    Initial commit

---
+++ src/main/java/ru/hopak/ilya/config/controllers/SecondController.java
@@ -0,0 +1,13 @@
+package ru.hopak.ilya.config.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+
+    @GetMapping("/exit")
+    public String exit(){
+        return "second/exit";
+    }
+}
